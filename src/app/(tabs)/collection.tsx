981e938d62430dabfffb2e983dc18f08a49628ce
Luigia Mihalcea <luigiamihaelam@gmail.com>
2026-06-05 19:12:11 +0300

    Refactor Title Style in Outfits and Collection Screens

--- src/app/(tabs)/collection.tsx
+++ src/app/(tabs)/collection.tsx
@@ -237,8 +237,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between', 
     alignItems: 'center', 
     paddingHorizontal: 24, 
-    paddingTop: 30,
-    paddingBottom: 20, 
+    marginVertical: 20, 
   },
   title: { 
     fontSize: 32, 
--- src/app/(tabs)/outfits.tsx
+++ src/app/(tabs)/outfits.tsx
@@ -213,8 +213,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between', 
     alignItems: 'center', 
     paddingHorizontal: 24, 
-    paddingTop: 30,
-    paddingBottom: 20, 
+    marginVertical: 20, 
   },
   title: { 
     fontSize: 32, 
